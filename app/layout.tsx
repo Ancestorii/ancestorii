b4ff817fb5fb56ab1a851353ac91b5ba46e35251
Ancestorii <igrindalone2@gmail.com>
2026-01-10 15:39:02 +0000

    Fix Loved Ones empty-state flicker and ensure members load correctly

--- app/layout.tsx
+++ app/layout.tsx
@@ -66,6 +66,28 @@ export const metadata: Metadata = {
   },
 };
 
+{/* 🔴 REDDIT PIXEL */}
+<Script id="reddit-pixel" strategy="afterInteractive">
+  {`
+!function(w,d){
+  if(!w.rdt){
+    var p=w.rdt=function(){
+      p.sendEvent ? p.sendEvent.apply(p,arguments) : p.callQueue.push(arguments)
+    };
+    p.callQueue=[];
+    var t=d.createElement("script");
+    t.src="https://www.redditstatic.com/ads/pixel.js";
+    t.async=!0;
+    var s=d.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(t,s);
+  }
+}(window,document);
+rdt('init','2_ibovuqrsg8y0');
+rdt('track','PageVisit');
+  `}
+</Script>
+
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
