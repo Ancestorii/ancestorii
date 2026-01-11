8216a9c7b64be746948e18820676c12da3e154be
Ancestorii <igrindalone2@gmail.com>
2026-01-11 09:16:01 +0000

    Fix Loved Ones empty-state flicker and ensure members load correctly

--- app/layout.tsx
+++ app/layout.tsx
@@ -70,6 +70,18 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
   <head>
+    {/* 🟡 GOOGLE TAG MANAGER */}
+<Script id="gtm-head" strategy="beforeInteractive">
+  {`
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id=GTM-PWNN6NGL'+dl;
+    f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-PWNN6NGL');
+  `}
+</Script>
+
     {/* 🔴 REDDIT PIXEL */}
     <Script id="reddit-pixel" strategy="afterInteractive">
       {`
@@ -111,6 +123,16 @@ rdt('track','PageVisit');
       <body
         className={`${inter.variable} antialiased text-gray-900 bg-transparent min-h-screen`}
       >
+        {/* 🟡 GOOGLE TAG MANAGER (noscript) */}
+<noscript>
+  <iframe
+    src="https://www.googletagmanager.com/ns.html?id=GTM-PWNN6NGL"
+    height="0"
+    width="0"
+    style={{ display: "none", visibility: "hidden" }}
+  />
+</noscript>
+
         {children}
 
         <Toaster
